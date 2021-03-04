d7c3d1aa4e85116073994b0b4ec3e9954204d687
Luca Barbato <lu_zero@gentoo.org>
2021-03-04 15:09:54 +0100

    by-gop parallel processing

---
+++ README.md
@@ -0,0 +1,5 @@
+# rav1e by-gop processing dataflow demo
+
+It will replace the current channel-api innards and provide parallelism at gop level.
+
+
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use crossbeam::channel::*;
 use crossbeam::thread;
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 
+use std::collections::BTreeMap;
+use std::sync::Arc;
 use std::thread::sleep;
 use std::time::Duration;
 
 struct F {
     idx: usize,
-    val: usize,
+    _val: usize,
 }
 
 const FRAME_CADENCE: Duration = Duration::from_millis(10);
@@ -21,13 +23,16 @@ fn producer(s: &thread::Scope, frames: usize, pb: ProgressBar) -> Receiver<F> {
     let (send, recv) = bounded(LOOKAHEAD * 2);
     s.spawn(move |_| {
         for idx in 0..frames {
-            let f = F { idx, val: idx % 42 };
+            let f = F {
+                idx,
+                _val: idx % 42,
+            };
 
             std::thread::sleep(FRAME_CADENCE);
 
             pb.set_message(&format!("Frame {}", idx));
             pb.inc(1);
-            send.send(f);
+            let _ = send.send(f);
         }
 
         pb.finish_with_message("Producer stopped");
@@ -82,7 +87,7 @@ fn scenechange(s: &thread::Scope, r: Receiver<F>, pb: ProgressBar) -> Receiver<S
 
                 pb.set_message(&format!("gop {} sub gop of {}", gop, lookahead.len()));
 
-                send.send(SB {
+                let _ = send.send(SB {
                     data: lookahead,
                     end_gop: end,
                 });
@@ -93,7 +98,7 @@ fn scenechange(s: &thread::Scope, r: Receiver<F>, pb: ProgressBar) -> Receiver<S
 
         assert!(lookahead.len() <= 7);
 
-        send.send(SB {
+        let _ = send.send(SB {
             data: lookahead,
             end_gop: false,
         });
@@ -108,42 +113,149 @@ struct Packet {
     idx: usize,
 }
 
-fn encode(s: &thread::Scope, r: Receiver<SB>, pb: ProgressBar) -> Receiver<Packet> {
-    let (send, recv) = unbounded();
-    s.spawn(move |_| {
-        for sb in r.iter() {
-            std::thread::sleep(INVARANTS_BUILD);
-            pb.set_message(&format!(
-                "Invariants built for {}..{}",
-                sb.data[0].idx,
-                sb.data.last().unwrap().idx
-            ));
+struct Worker {
+    idx: usize,
+    back: Sender<usize>,
+    send: Sender<Packet>,
+    pb: ProgressBar,
+}
 
-            for f in sb.data {
-                sleep(RDO);
-                pb.inc(1);
-                send.send(Packet { idx: f.idx });
-            }
+impl Worker {
+    fn process(&self, sb: SB) {
+        std::thread::sleep(INVARANTS_BUILD);
+        self.pb.set_message(&format!(
+            "Processing {}..{}",
+            sb.data[0].idx,
+            sb.data.last().unwrap().idx
+        ));
 
-            if sb.end_gop {
-                pb.set_message(&format!("starting a new gop"));
-            }
+        for f in sb.data {
+            sleep(RDO);
+            self.pb.inc(1);
+            let _ = self.send.send(Packet { idx: f.idx });
+        }
+    }
+}
+
+impl Drop for Worker {
+    fn drop(&mut self) {
+        let _ = self.back.send(self.idx);
+    }
+}
+
+struct WorkerPool {
+    send_workers: Sender<usize>,
+    recv_workers: Receiver<usize>,
+    send_reassemble: Sender<(usize, Receiver<Packet>)>,
+    m: Arc<MultiProgress>,
+    count: usize,
+}
+
+impl WorkerPool {
+    fn new(workers: usize, m: Arc<MultiProgress>) -> (Self, Receiver<(usize, Receiver<Packet>)>) {
+        let (send_workers, recv_workers) = bounded(workers);
+        let (send_reassemble, recv_reassemble) = unbounded();
+
+        for w in 0..workers {
+            let _ = send_workers.send(w);
         }
 
-        pb.finish_with_message("Complete");
+        (
+            WorkerPool {
+                send_workers,
+                recv_workers,
+                send_reassemble,
+                m,
+                count: 0,
+            },
+            recv_reassemble,
+        )
+    }
+
+    fn get_worker(&mut self, s: &thread::Scope) -> Option<Sender<SB>> {
+        self.recv_workers.recv().ok().map(|idx| {
+            let (sb_send, sb_recv) = unbounded();
+            let (send, recv) = unbounded();
+
+            let _ = self.send_reassemble.send((self.count, recv));
+
+            let spinner_style = ProgressStyle::default_spinner()
+                .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ")
+                .template("  {prefix:.bold.dim} {spinner} {msg}");
+
+            let pb = self.m.add(ProgressBar::new(!0).with_style(spinner_style));
+
+            let w = Worker {
+                idx,
+                back: self.send_workers.clone(),
+                send,
+                pb,
+            };
+
+            s.spawn(move |_| {
+                for sb in sb_recv.iter() {
+                    w.process(sb);
+                }
+            });
+
+            self.count += 1;
+
+            sb_send
+        })
+    }
+}
+
+fn reassemble(
+    recv_reassemble: Receiver<(usize, Receiver<Packet>)>,
+    s: &thread::Scope,
+) -> Receiver<Packet> {
+    let (send_packet, receive_packet) = unbounded();
+
+    s.spawn(move |_| {
+        let mut pending = BTreeMap::new();
+        let mut last_idx = 0;
+        for (idx, recv) in recv_reassemble.iter() {
+            pending.insert(idx, recv);
+            while let Some(recv) = pending.remove(&last_idx) {
+                for p in recv {
+                    let _ = send_packet.send(p);
+                }
+            }
+            last_idx += 1;
+        }
     });
 
-    recv
+    receive_packet
+}
+
+fn encode(s: &thread::Scope, r: Receiver<SB>, m: Arc<MultiProgress>) -> Receiver<Packet> {
+    let (mut pool, recv) = WorkerPool::new(4, m);
+
+    let mut sb_send = pool.get_worker(s).unwrap();
+
+    s.spawn(move |s| {
+        for sb in r.iter() {
+            let end_gop = sb.end_gop;
+
+            let _ = sb_send.send(sb);
+
+            if end_gop {
+                sb_send = pool.get_worker(s).unwrap();
+            }
+        }
+    });
+
+    reassemble(recv, s)
 }
 
 fn main() {
-    let m = MultiProgress::new();
+    let m = Arc::new(MultiProgress::new());
 
     let spinner_style = ProgressStyle::default_spinner()
         .tick_chars("⠁⠂⠄⡀⢀⠠⠐⠈ ")
         .template("{prefix:.bold.dim} {spinner} {msg}");
 
-    thread::scope(|s| {
+    let _ = thread::scope(|s| {
         let pb = m.add(ProgressBar::new(600));
         pb.set_style(spinner_style.clone());
         let f_recv = producer(s, 600, pb);
@@ -151,11 +263,10 @@ fn main() {
         let pb = m.add(ProgressBar::new(!0).with_style(spinner_style.clone()));
         let sb_recv = scenechange(s, f_recv, pb);
 
-        let pb = m.add(ProgressBar::new(!0).with_style(spinner_style.clone()));
-        let packet_recv = encode(s, sb_recv, pb);
+        let m2 = Arc::clone(&m);
+        let packet_recv = encode(s, sb_recv, m2);
 
         let pb = m.add(ProgressBar::new(!0).with_style(spinner_style.clone()));
-
         s.spawn(move |_| {
             for p in packet_recv.iter() {
                 pb.set_message(&format!("Packet {}", p.idx));
